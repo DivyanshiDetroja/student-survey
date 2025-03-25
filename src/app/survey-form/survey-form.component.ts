2fb1aac41ad06ab0106e51ba66838d2e6a7a2946
Divyanshi Detroja <107313235+DivyanshiDetroja@users.noreply.github.com>
2025-03-24 23:09:37 -0400

    Add files via upload

--- src/app/survey-form/survey-form.component.ts
+++ src/app/survey-form/survey-form.component.ts
@@ -34,109 +34,166 @@ export class SurveyFormComponent {
     private http: HttpClient
   ) {
     this.surveyForm = this.fb.group({
-      firstName: ['' ],
-      lastName: [''],
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)] ],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
       streetAddress: [''],
-      zip: [''],
+      zip: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
       city: ['' ],
       state: ['' ],
-      telephone: [''],
-      email: [''],
-      date: [''],
-      interestSource: [''],
-      likelihood: [''],
-      comments: ['']
+      telephone: ['', [Validators.required, Validators.pattern(/^\d{3}-\d{3}-\d{4}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      date: ['', Validators.required],
+      interestSource: ['' , Validators.required],
+      likelihood: ['' , Validators.required],
+      comments: [''],
     });
   }
 
-  // ngOnInit() {
-  //   // Load the zip code data from the JSON file
-  //   this.http.get<any>('assets/zipcodes.json').subscribe({
-  //     next: data => {
-  //       this.zipCodeData = data.zipcodes; // Extract the zipcodes array
-  //     },
-  //     error: error => {
-  //       console.error('Error loading zip code data:', error);
-  //     }
-  //   });
-    
+  
+  ngOnInit() {
+    this.http.get<any>('zipcodes.json').subscribe({
+      next: (data) => {
+        this.zipCodeData = data.zip_codes;
+        console.log('Received data:', this.zipCodeData);
+      },
+      error: (error) => {
+        
+        console.error('Error loading zip code data:', error);
+      },
+    });
 
-  //   // Subscribe to zip code value changes
-  //   this.zipSubscription = this.surveyForm.get('zip')?.valueChanges.subscribe(zip => {
-  //     if (zip && zip.length === 5) {
-  //       this.lookupZipCode(zip);
-  //     } else {
-  //       // Clear city and state if zip is invalid
-  //       this.surveyForm.patchValue({ city: '', state: '' });
-  //     }
-  //   });
-  // }
+    this.zipSubscription = this.surveyForm.get('zip')?.valueChanges.subscribe((zip) => {
+      if (zip && zip.length === 5) {
+        this.lookupZipCode(zip); // You will need to change lookupZipCode function to access zip_code property.
+      } else {
+        this.surveyForm.patchValue({ city: '', state: '' });
+      }
+    });
+  }
 
   ngOnDestroy() {
-    // Unsubscribe to prevent memory leaks
     if (this.zipSubscription) {
       this.zipSubscription.unsubscribe();
     }
   }
 
-  // lookupZipCode(zip: string) {
-  //   const zipInfo = this.zipCodeData.find(item => item.zip === zip);
-  //   if (zipInfo) {
-  //     // Update city and state form controls
-  //     this.surveyForm.patchValue({
-  //       city: zipInfo.city,
-  //       state: zipInfo.state
-  //     });
-  //   } else {
-  //     // Clear city and state if zip code not found
-  //     this.surveyForm.patchValue({ city: '', state: '' });
-  //   }
-  // }
+  lookupZipCode(zip: string) {
+    const zipInfo = this.zipCodeData.find((item) => item.zip_code === Number(zip)); // Corrected property name and type conversion
   
-
+    if (zipInfo) {
+      this.surveyForm.patchValue({
+        city: zipInfo.city,
+        state: zipInfo.state,
+      });
+    } else {
+      this.surveyForm.patchValue({ city: '', state: '' });
+    }
+  }
   get formattedCampusLikes() {
     return this.selectedCampusLikes.join(',');
   }
 
-  // Method to handle changes in campus likes checkboxes
   onCampusLikeChange(event: any) {
     const value = event.target.value;
     if (event.target.checked) {
       this.selectedCampusLikes.push(value);
     } else {
-      this.selectedCampusLikes = this.selectedCampusLikes.filter(item => item !== value);
+      this.selectedCampusLikes = this.selectedCampusLikes.filter((item) => item !== value);
     }
   }
 
   onSubmit() {
-    if (this.surveyForm.valid) {
+    if (this.surveyForm.valid && this.selectedCampusLikes.length >= 2) {
       const surveyData = {
         ...this.surveyForm.getRawValue(),
-        campusLikes: this.formattedCampusLikes
+        campusLikes: this.formattedCampusLikes,
       };
-      // Send data to the backend server
-      this.http.post('http://localhost:8080/surveys', surveyData)
-        .subscribe({
-          next: response => {
-            console.log('Survey submitted successfully!', response);
-            alert('Survey submitted successfully!');
-            this.router.navigate(['/']);
-          },
-          error: error => {
-            console.error('Error submitting survey:', error);
-            alert('There was an error submitting the survey.');
-            // console.log(surveyData);
-          }
-        });
+      this.http.post('http://localhost:8080/surveys', surveyData).subscribe({
+        next: (response) => {
+          console.log('Survey submitted successfully!', response);
+          alert('Survey submitted successfully!');
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          console.error('Error submitting survey:', error);
+          alert('There was an error submitting the survey.');
+          console.log(surveyData);
+        },
+      });
     } else {
-      alert('Please fill in all required fields.');
+      const errorMessages: string[] = [];
+      if (this.selectedCampusLikes.length < 2) {
+        errorMessages.push('Please select at least 2 campus likes.');
+      }
+      Object.keys(this.surveyForm.controls).forEach((field) => {
+        const control = this.surveyForm.get(field);
+        if (control?.invalid && control.touched) {
+          let errorMessage = '';
+          switch (field) {
+            case 'firstName':
+              if (control.errors?.['required']) {
+                errorMessage = 'First name is a required field.';
+              } else if (control.errors?.['pattern']) {
+                errorMessage = 'First name must contain only letters and spaces.';
+              }
+              break;
+            case 'lastName':
+              if (control.errors?.['required']) {
+                errorMessage = 'Last name is a required field.';
+              } else if (control.errors?.['pattern']) {
+                errorMessage = 'Last name must contain only letters and spaces.';
+              }
+              break;
+            case 'zip':
+              if (control.errors?.['required']) {
+                errorMessage = 'Zip code is a required field.';
+              } else if (control.errors?.['pattern']) {
+                errorMessage = 'Zip code must be 5 digits.';
+              }
+              break;
+            case 'telephone':
+              if (control.errors?.['required']) {
+                errorMessage = 'Telephone number is a required field.';
+              } else if (control.errors?.['pattern']) {
+                errorMessage = 'Invalid telephone number format (e.g., 123-456-7890).';
+              }
+              break;
+            case 'email':
+              if (control.errors?.['required']) {
+                errorMessage = 'Email is a required field.';
+              } else if (control.errors?.['email']) {
+                errorMessage = 'Invalid email format.';
+              }
+              break;
+            case 'date':
+              errorMessage = 'Date of Survey is a required field.';
+              break;
+            case 'interestSource':
+              errorMessage = 'How did you become interested in the university? is a required field.';
+              break;
+            case 'likelihood':
+              errorMessage = 'Likelihood of recommending this school to others is a required field.';
+              break;
+            default:
+              errorMessage = `${field} is invalid.`;
+          }
+          if (errorMessage) {
+            errorMessages.push(errorMessage);
+          }
+        }
+      });
+      alert(errorMessages.join('\n'));
+    }
+  }
+
+  onZipBlur() {
+    const zip = this.surveyForm.get('zip')?.value;
+    if (zip && zip.length === 5) {
+      this.lookupZipCode(zip);
     }
   }
 
   onCancel() {
     this.router.navigate(['/']);
-  }
+  }
 }
-
-
-
